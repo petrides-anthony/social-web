f510fee2dc60d29e32fbe166086d0a1a99e559f9
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2020-07-27 17:05:36 +0100

    initial skeleton commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './scss/app.scss'
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from './components/Header/Header'
+
+class HelloMessage extends React.Component {
+    render() {
+        return <div>
+            <Header/>
+            <div className="container">
+                <h1>Hello {this.props.name}</h1>
+            </div>
+        </div>
+    }
+}
+
+let App = document.getElementById("app");
+
+ReactDOM.render(<HelloMessage name="Yomi" />, App);
