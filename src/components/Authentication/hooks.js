8f2ac28ba9a7704edb4bfeb8e4b99d7e1d7cb69f
Luca Colonnello <luca.colonnello@thetrainline.com>
2020-08-11 07:51:44 +0100

    Add Authentication component module

---
+++ src/components/Authentication/hooks.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import AuthenticationContext from './AuthenticationContext';
+
+const useIsAuthenticated = () => {
+  const { authenticationState } = React.useContext(AuthenticationContext);
+  return authenticationState.authenticated;
+};
+
+const useLogin = () => {
+  const { dispatch } = React.useContext(AuthenticationContext);
+
+  const loginFromHook = username => {
+    const loginAction = { type: 'login', payload: { username } };
+    dispatch(loginAction);
+  };
+
+  return loginFromHook;
+};
+
+const useLogout = () => {
+  const { dispatch } = React.useContext(AuthenticationContext);
+  
+  const logoutFromHook = () => {
+    const logoutAction = { type: 'logout' };
+    dispatch(logoutAction);
+  };
+
+  return logoutFromHook;
+};
+
+export {
+  useIsAuthenticated,
+  useLogin,
+  useLogout,
+};
