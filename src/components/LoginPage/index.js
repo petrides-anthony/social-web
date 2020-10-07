0c47421d7bb257ac9658f67423340055d8ae6720
Anthony Petrides <anthony.petrides@thetrainline.com>
2020-10-07 18:24:35 +0100

    add validation to login input

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -8,6 +8,7 @@ import NavBar from "../OnPageComponents/NavBar";
 
 const LoginPage = () => {
   const [inputValue, setInputValue] = useState('');
+  const [error, setError] = useState(false);
 
   const history = useHistory();
   const isAuthenticated = useIsAuthenticated();
@@ -28,13 +29,39 @@ const LoginPage = () => {
 
   const login = useLogin();
 
+  const onLoginButtonClick = () => {
+    if (!inputValue) {
+      setError(true);
+      return;
+    }
+
+    login(inputValue);
+  };
+
+  const onInputChange = (e) => {
+    setError(false);
+    setInputValue(e.target.value);
+  };
+
   return (
     <div>
       <NavBar />
         <div className="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
           <h1 className="block w-full text-center text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase mb-6">Login Page</h1>
-          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" value={inputValue} onChange={e => setInputValue(e.target.value)} type="text" placeholder="Tell me your username" />
-          <Button onClick={() => login(inputValue)}>
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            value={inputValue}
+            onChange={onInputChange}
+            type="text"
+            placeholder="Tell me your username"
+          />
+
+          {error ? (
+            <p className="text-red-700 italic">
+              You need to specify a username in order to login.
+            </p>
+          ) : null}
+          <Button onClick={onLoginButtonClick}>
             {inputValue ? `Login as ${inputValue}` : 'Login'}
           </Button>
       </div>
--- src/components/OnPageComponents/Button/index.js
+++ src/components/OnPageComponents/Button/index.js
@@ -12,4 +12,3 @@ const Button = (props) => {
 };
 
 export default Button;
-
