0d99ad080f1d15b53e1d726f767d073a5c7cd5f5
Anthony Petrides <anthony.petrides@thetrainline.com>
2020-10-18 16:17:19 +0100

    remove post container and add to post list

--- src/components/common/PostList/index.js
+++ src/components/common/PostList/index.js
@@ -26,12 +26,63 @@ const PostList = (props) => {
   const postLikes = postData.likes;
 
   return (
-
     <div>
-        <p>- User: {postUser}</p>
-        <p>- Date Posted: {postDate}</p>
-        <p>- Content: {postContent}</p>
-        <p>- Likes: {postLikes}</p>
+
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+        <div className="px-4 py-5 border-b border-gray-200 sm:px-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">
+            Posts
+          </h3>
+          <p className="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
+            Full Post details below
+          </p>
+        </div>
+        <div>
+          <dl>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm leading-5 font-medium text-gray-500">
+                Username
+              </dt>
+              <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                {postUser}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm leading-5 font-medium text-gray-500">
+                Date Posted
+              </dt>
+              <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                {postDate}
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm leading-5 font-medium text-gray-500">
+                Content
+              </dt>
+              <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                {postContent}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm leading-5 font-medium text-gray-500">
+                Likes
+              </dt>
+              <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                {postLikes} people liked this
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm leading-5 font-medium text-gray-500">
+                Comments
+              </dt>
+              <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                Functionality TBC
+              </dd>
+            </div>
+          </dl>
+        </div>
+        </div>
+
     </div>
   );
 };
--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -5,7 +5,6 @@ import { useLogout, useUsername } from "../../utils/Authentication";
 import NavBar from "../../common/NavBar";
 import Button from "../../common/Button";
 import PostList from "../../common/PostList";
-import PostContainer from "../../common/PostContainer";
 
 const HomePage = () => {
   const logout = useLogout();
@@ -14,7 +13,7 @@ const HomePage = () => {
   return (
     <div>
     <NavBar/>
-    <div className="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
+    <div className="content-center w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
       <div className="container mx-auto px-4">
         <p className="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase mt-4">Welcome Home {username}</p>
         <Button onClick={() => logout()}>
@@ -24,9 +23,6 @@ const HomePage = () => {
       </div>
       <div className="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
       <PostList user="0"/>
-      </div>
-      <div className="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
-      <PostContainer/>
       </div>
     </div>
   );
